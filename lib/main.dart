6a8b5f8aa694824b4700b2aff46e89222810ec2b
origogi <rlawjdxo1212@naver.com>
2020-12-09 11:06:02 +0900

    fix minSDK 16 to 19

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,54 +14,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: KakaoLoginText(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class KakaoLoginText extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _KakaoLoginTextState createState() => _KakaoLoginTextState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _KakaoLoginTextState extends State<KakaoLoginText> {
+  @override
+  void initState() {
+    super.initState();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    KakaoContext.clientId = "276149c5b6a7ec7b89af7ebc8931e7de";
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text('Kako Test app')),
     );
   }
 }
