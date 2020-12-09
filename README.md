9510bffad5b3f7994fb98fb62e9ad241996da9ca
origogi <rlawjdxo1212@naver.com>
2020-12-09 10:28:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# kakao_sdk_sample
+
+Test flutter Kakao sdk
+
+## SDK
+
+[Link](https://pub.dev/packages/kakao_flutter_sdk)
---
+++ android/app/src/main/kotlin/com/example/kakao_sdk_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kakao_sdk_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
